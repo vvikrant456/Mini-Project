8dfce41b16690daa2c54f79f2a995c86196ff734
VikasPc <vvikrant456@gmail.com>
2021-09-25 12:10:29 +0530

    Getting Nth sequence

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,96 +1,36 @@
 #include <stdio.h>
 #define ll long long
-
-ll fib91()
+int main()
 {
-    ll t1 = 0;
-    ll t2 = 1;
-    ll res;
-
-    for (int i = 1; i <= 91; i++)
-    {
-        res = t1 + t2;
-        t1 = t2;
-        t2 = res;
-    }
-    return t1;
-}
-
-ll fib92()
-{
-    ll t1 = 0;
-    ll t2 = 1;
-    ll res;
-
-    for (int i = 1; i <= 92; i++)
-    {
-        res = t1 + t2;
-        t1 = t2;
-        t2 = res;
-    }
-    return t1;
-}
-
-ll main()
-{
-    ll t1 = 0;
-    ll t2 = 1;
-    ll res;
     int n;
     scanf("%d", &n);
-    if (n <= 92)
+
+    ll t1[500] = {0};
+    ll t2[500] = {0};
+    t2[499] = 1;
+    ll N_thTerm[500] = {0};
+    for (int i = 1; i <= n; i++)
     {
-        for (int i = 1; i <= n; i++)
-        {
-            res = t1 + t2;
-            t1 = t2;
-            t2 = res;
-        }
-
-        printf("%lld\n", t1);
-    }
-
-    if (n > 92)
-    {
-
-        ll fib_91 = fib91();
-        ll fib_92 = fib92();
-
-        int num1[400], num2[400];
-        for (int i = 0; i < 400; i++)
-        {
-            num1[i] = 0;
-            num2[i] = 0;
-        }
-        int j = 399;
-        while (fib_91)
-        {
-            num1[j] = fib_91 % 10;
-            fib_91 /= 10;
-            j--;
-        }
-        j = 399;
-
-        while (fib_92)
-        {
-            num2[j] = fib_92 % 10;
-            fib_92 /= 10;
-            j--;
-        }
-
-        int nextFib[400] = {0}; //92
         int carry = 0;
-        for (int i = 399; i >= 0; i--)
+        for (int j = 499; j >= 0; j--)
         {
-            nextFib[i] = (num1[i] + num2[i]) % 10 + carry;
-
-            carry = (num1[i] + num2[i]) / 10;
+            N_thTerm[j] = (t1[j] + t2[j]) % 10 + carry;
+            // printf("%d", N_thTerm[j]);
+            carry = (t1[j] + t2[j]) / 10;
         }
+        // printf("\n");
 
-        for (int i = 0; i < 400; i++)
+        for (int k = 499; k >= 0; k--)
         {
-            printf("%d", nextFib[i]);
+            t1[k] = t2[k];
+            t2[k] = N_thTerm[k];
         }
     }
+
+    for (int i = 0; i < 500; i++)
+    {
+        printf("%d", t1[i]);
+    }
+
     return 0;
 }
